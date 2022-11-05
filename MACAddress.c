fe01334df8556e0654696a70f13caa7c682ba131
Stanislav Vodolagin <ximtech@inbox.lv>
2022-11-05 13:13:30 +0200

    Mac address parse fix

--- MACAddress.c
+++ MACAddress.c
@@ -22,7 +22,7 @@ MACAddress macAddressFromString(const char *address) {
         }
 
         uint32_t macBytes[MAC_ADDRESS_OCTETS_COUNT];
-        int32_t conversionResult = sscanf(address, "%2lX:%2lX:%2lX:%2lX:%2lX:%2lX", &macBytes[0], &macBytes[1], &macBytes[2], &macBytes[3], &macBytes[4], &macBytes[5]);
+        int32_t conversionResult = sscanf(address, "%x:%x:%x:%x:%x:%x", &macBytes[0], &macBytes[1], &macBytes[2], &macBytes[3], &macBytes[4], &macBytes[5]);
         if (conversionResult != MAC_ADDRESS_OCTETS_COUNT) {
             return macAddress;
         }
